ff048d05aa22c779f3c607790f0043c2bb52440e
Nafisa <nafisar00@gmail.com>
2023-03-18 19:55:40 +1100

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Nafisa Rahman on 18/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
